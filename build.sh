633aa92b37941f5e82ef312ceb3407146797e198
Lars Kanis <kanis@comcard.de>
2020-07-21 17:20:26 +0200

    Daemon-Mode mit dokumentiert

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ cp ~/I/PROG/_post/Mailoptimizer/MO_Installer.exe .
 wine MO_Installer.exe
 docker build . --build-arg http_proxy --build-arg https_proxy --tag mailoptimizer
 
-# Run per:
-
+# Run interactive per:
 docker run -it --rm -p 2511:2511 -p 8765:8765 -v mailoptimizer-kunden:/opt/mailoptimizer/Kunden -v mailoptimizer-mysql:/var/lib/mysql mailoptimizer
 
+# Run as daemon per:
+docker run -p 2511:2511 -p 8765:8765 -v mailoptimizer-kunden:/opt/mailoptimizer/Kunden -v mailoptimizer-mysql:/var/lib/mysql -d --restart unless-stopped --memory-swappiness=0 mailoptimizer
