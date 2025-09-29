4137e66dbdc75101934602663c35f316dafbec20
Lars Kanis <lars.kanis@sincnovation.com>
2025-09-29 11:44:33 +0200

    Installation über simulierte Konsoleneingaben funktioniert nicht ab Mailoptimizer-5.12
    Die Variante über prop-Datei funktioniert aber zumindest ab Mailoptimizer-5.11

--- startup.sh
+++ startup.sh
@@ -18,31 +18,46 @@ echo "
 # Netzwerk temporär abschalten, damit Installer schneller läuft
 iptables -A OUTPUT -p tcp --destination-port 443 -j REJECT
 
-# Installation über simulierte Konsoleneingaben:
-echo -e "1\n\
-1\n\
-1\n\
-/mo-installer/2022-02-23_Mo_config_15420_5046577760.xml\n\
-1\n\
-2511\n\
-80\n\
-mailoptimizer\n\
-mailoptimizer\n\
-/\n\
-MySQL via JDBC\n\
-jdbc:mysql://mysql:3306/mo?useSSL=false&serverTimezone=CET\n\
-mo\n\
-mopw\n\
-1\n\
-\n\
-1\n\
-/\n\
-y\n\
-\n\
-" | bash /mo-installer/Setup_MO_x32_x64.sh -c -Dinstall4j.keepLog=true -Dinstall4j.debug=false -Dinstall4j.alternativeLogfile=/mo-unattended.log || (cat /mo-unattended.log; false)
+# Installation über simulierte Konsoleneingaben funktioniert nicht ab Mailoptimizer-5.12:
+# echo -e "1\n\
+# 1\n\
+# 1\n\
+# /mo-installer/2022-02-23_Mo_config_15420_5046577760.xml\n\
+# 1\n\
+# 2511\n\
+# 80\n\
+# mailoptimizer\n\
+# mailoptimizer\n\
+# /\n\
+# MySQL via JDBC\n\
+# jdbc:mysql://mysql:3306/mo?useSSL=false&serverTimezone=CET\n\
+# mo\n\
+# mopw\n\
+# 1\n\
+# \n\
+# 1\n\
+# /\n\
+# y\n\
+# \n\
+# " | bash /mo-installer/Setup_MO_x32_x64.sh -c -Dinstall4j.keepLog=true -Dinstall4j.debug=false -Dinstall4j.alternativeLogfile=/mo-unattended.log || (cat /mo-unattended.log; false)
 
-# Die Variante über prop-Datei funktioniert leider nicht -> Abbruch ohne Fehlermeldung:
-# bash /mo-installer/Setup_MO_x32_x64.sh -q -console -varfile /mo-installer/MO_Classic_Installation_Linux.prop -Dinstall4j.keepLog=true -Dinstall4j.debug=true
+# Die Variante über prop-Datei funktioniert aber zumindest ab Mailoptimizer-5.11:
+cat <<EOT >> /mo-install.prop
+sys.languageId=de
+mailoptimizer.produkt=0
+mailoptimizer.installationDir=/opt/mailoptimizer
+mailoptimizer.keyfile=/mo-installer/2022-02-23_Mo_config_15420_5046577760.xml
+mailoptimizer.datenbank.typ=5
+mailoptimizer.datenbank.user=mo
+mailoptimizer.datenbank.passwort=mopw
+mailoptimizer.datenbank.url=jdbc:mysql://mysql:3306/mo?useSSL=false&serverTimezone=CET
+mailoptimizer.simulation=false
+mailoptimizer.dbbackup.done=true
+mailoptimizer.filebackup.create=false
+EOT
+# Installation oder Update durchführen
+# Wenn aktuelle Version bereits installiert, dann trotzdem fortfahren, anderenfalls Abbruch bei Fehler
+(bash /mo-installer/Setup_MO_x32_x64.sh -q -console -varfile /mo-install.prop -Dinstall4j.keepLog=true -Dinstall4j.debug=true -Dinstall4j.alternativeLogfile=/mo-unattended.log | tee /mo-install.log ; exit ${PIPESTATUS[0]} ) || grep "Die gefundene Mailoptimizer-Version ist mit dem Installer identisch." /mo-install.log || (cat /mo-unattended.log; false)
 
 # Netzwerk nach Installation wieder einschalten
 iptables -D OUTPUT -p tcp --destination-port 443 -j REJECT
